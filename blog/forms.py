f2f67e4d10e1ee1798d2d40bea2f0d34be1a5664
acecrosser <acecrosser@yandex.ru>
2021-03-01 13:51:28 +0300

    page add post

---
+++ blog/forms.py
@@ -0,0 +1,18 @@
+from django.forms import ModelForm
+from django.forms.widgets import Select, Textarea
+from .models import BlogPost
+
+
+class PostForm(ModelForm):
+
+    class Meta:
+        model = BlogPost
+        fields = ('title', 'body', 'tags', 'rubric_name', 'slug')
+        labels = {
+            'title': 'Заголовок записи',
+            'body': 'Текст записи'
+        }
+        widgets = {
+            'body': Textarea(attrs={'cols': 80, 'rows': 20}),
+            'rubric_name': Select(attrs={'size': 5})
+        }
